c3662ef4f0f478da4a35010c821566c0b22321a0
Rafael-P <r4f4el_porto@hotmail.com>
2020-11-17 12:22:56 -0300

    exercícios adicionados

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Laços_de_Repetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //do while pergunte a idade até acertar
+
+            // int idade;
+            // do{
+            //     Console.WriteLine("Digite a idade do Neymar");
+            //     idade = int.Parse(Console.ReadLine());
+            // }while(idade != 28);
+            // Console.WriteLine("Acertou Neymarzete");
+
+            
+
+            //while Pergunte a idade até acertar
+
+            // Console.WriteLine("Digite a idade do Neymar");
+            // int idade = int.Parse(Console.ReadLine());
+
+            // while(idade != 28){
+            //     Console.WriteLine("Errou!");
+            //     Console.WriteLine("Digite a idade do Ney novamente");
+            //     idade = int.Parse(Console.ReadLine());
+            // }
+            // Console.WriteLine("Agora sim!");
+
+
+
+            //for 0 até 50
+
+            // for (int i = 0; i <= 50; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+
+
+            //for 200 até 100 decrescente
+
+            // for (int i = 200; i >= 100; i--)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+
+        }//FIM DO MAIN
+    }
+}
